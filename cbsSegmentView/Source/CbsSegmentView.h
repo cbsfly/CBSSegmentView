33d35dc0fc0971a0900964e44f412ad177046944
a502332019 <cbs_950320@qq.com>
2016-04-30 19:12:03 +0800

    1231

--- cbsSegmentView/Source/CbsSegmentView.h
+++ cbsSegmentView/Source/CbsSegmentView.h
@@ -12,20 +12,26 @@ typedef NS_ENUM(NSInteger, CbsSegmentHeaderType) {
     CbsSegmentHeaderTypeFixed,
     CbsSegmentHeaderTypeScroll
 };
-@protocol CbsSegmentViewDelegate <NSObject>
 
+
+@protocol CbsSegmentViewDelegate <NSObject>
+//点击标签栏按钮代理方法
 - (void)didSelectSegmentIndex:(NSInteger)index;
 
 @end
 
 @interface CbsSegmentView : UIView
+@property (nonatomic, weak) id<CbsSegmentViewDelegate> delegate;
+//标签栏颜色
 @property (nonatomic, strong) UIColor *headerColor;
+//非选中状态下标签字体颜色
 @property (nonatomic, strong) UIColor *titleColor;
+//选中标签字体颜色
 @property (nonatomic, strong) UIColor *titleSelectedColor;
+//选中标签底部划线颜色
 @property (nonatomic, strong) UIColor *bottomLineColor;
 
-@property (nonatomic, weak) id<CbsSegmentViewDelegate> delegate;
-
+//初始化方法
 - (instancetype)initWithFrame:(CGRect)frame title:(NSArray *)titleArray view:(NSArray *)viewArray type:(CbsSegmentHeaderType)type;
 
 @end
